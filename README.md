704d3b4afafdc92249fc24d960f240bef08bd06d
disturbing <joseph@kintohub.com>
2020-01-24 21:26:56 +0800

    :alien: feat(Session): Restructured project and fixed sessions to work with latest actions
    * Added readme

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
 # Session Graphql Actions
 
-Basic JWT Session Management
+GraphQL Actions is a new concept about attaching logic to graphql queries. This action will create a JWT session for Hasura.
+
+## Warning
+
+Right now actions is in a **preview** state. We have included the custom utilities and cli tools in hasura-migrations folder.
+
+Once actions is out of preview, will standardize this repository to work with a standard hasura graphql engine version
+
+## Project Structure
+
+Using one of the standard go project formats, we have two apps and the database migrations required.
+
+* `/cmd/cleanup-sessions-job` a go app to cleanup expired sessions as a cron job
+* `/cmd/session-service` a go microservice to create JWT tokens specifically for hasura graphql engine
+* `/hasura-migrations` a custom job to apply hasura database migrations for the services in this project
+* `/internal` common code for go apps
+* `docker-compose.yaml` sets up hasura, postgres and applies migrations to them
+
+## How to use
+
+* Install go 1.12 or higher
+* Install docker 2.x or higher
+* Run `docker-compose up -d`
+* Run `go mod download && go run ./cmd/session-service`
+* Access hasura a http://localhost:8080
+
+## Env Vars
+
+Read the .env-example comments. Copy .env-example as a .env file in the root of this repository
+to setup local env variables for testing.
+
+## Functionality
+
+This project is purely for creating generic permission based sessions that can expire.
+
+**Authenticated users can query their own sessions**
+
+```
+query {
+   sessions {
+      id
+      createdAt
+      expiresAt
+      ownerId
+      claims
+   }
+}
+```
+
+**Admins (backend services) can create sessions**
+
+```
+mutation {
+  createSession(
+    defaultRole: "user",
+    expiresAt:"2020-01-23T07:00:27+00:00",
+    ownerId: "395e99fe-e075-4f30-8ef7-dd71bc9c98d5") {
+    accessToken
+  }
+}
+
+```
+
+Admins can also CRUD the entire sessions table itself.
---
+++ cmd/session-service/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/go-ozzo/ozzo-validation/v3"
+	"github.com/kintohub/session-graphql-actions/internal/config"
+	"github.com/machinebox/graphql"
+	"github.com/pkg/errors"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	hasuraClient *graphql.Client
+)
+
+type HasuraAction struct {
+	SessionVariables map[string]string `json:"session_variables"`
+	Input            ActionInput       `json:"input"`
+}
+
+func (i HasuraAction) Validate() error {
+	return validation.ValidateStruct(&i,
+		validation.Field(&i.Input, validation.Required))
+}
+
+type ActionInput struct {
+	ExpiresAt   string `json:"expiresAt"`
+	OwnerId     string `json:"ownerId"`
+	DefaultRole string `json:"defaultRole"`
+}
+
+func (i ActionInput) Validate() error {
+	return validation.ValidateStruct(&i,
+		validation.Field(&i.OwnerId, validation.Required),
+		validation.Field(&i.ExpiresAt, validation.Required),
+		validation.Field(&i.DefaultRole, validation.Required, validation.By(validDefaultRole)),
+	)
+}
+
+type Claims struct {
+	jwt.StandardClaims
+	Hasura HasuraClaims `json:"hasuraClient"`
+}
+
+type HasuraClaims struct {
+	AllowedRoles []string `json:"x-hasuraClient-allowed-roles"`
+	DefaultRole  string   `json:"x-hasuraClient-default-role"`
+	SessionId    string   `json:"x-hasuraClient-session-id"`
+	OwnerId      string   `json:"x-hasuraClient-owner-id"`
+}
+
+type InsertSessionResponse struct {
+	InsertedSessions struct {
+		Returning []struct {
+			Id        string `json:"id"`
+			CreatedAt string `json"createdAt"`
+			ExpiresAt string `json"expiresAt"`
+		} `json:"returning"`
+	} `json:"insert_sessions"`
+}
+
+func main() {
+	hasuraClient = graphql.NewClient(config.HasuraGraphqlEndpoint)
+
+	r := gin.Default()
+	r.POST("/createSession", createSessionHandler)
+	r.Run(":3000")
+}
+
+func validDefaultRole(role interface{}) error {
+	for _, hasuraRole := range config.HasuraRoles {
+		if hasuraRole == role {
+			return nil
+		}
+	}
+
+	return errors.New(fmt.Sprintf("Provided invalid role. Valid roles are %v", config.HasuraRoles))
+}
+
+func createSessionHandler(c *gin.Context) {
+	var request HasuraAction
+
+	if err := c.BindJSON(&request); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"errors": gin.H{
+			"message": "Invalid json object: " + err.Error(),
+		}})
+	} else if err := request.Validate(); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"errors": gin.H{"message": err.Error()}})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"data": map[string]string{
+				"accessToken": createAccessToken(&request.Input),
+			},
+		})
+	}
+}
+
+func createAccessToken(input *ActionInput) string {
+	req := graphql.NewRequest(`
+		mutation CreateSession($ownerId: uuid, $expiresAt: timestamptz) {
+		  insert_sessions(objects: {ownerId: $ownerId, expiresAt: $expiresAt}) {
+			returning {
+			  id
+			  createdAt
+              expiresAt
+			}
+		  }
+		}`)
+
+	req.Var("ownerId", input.OwnerId)
+	req.Var("expiresAt", input.ExpiresAt)
+	req.Header.Set("X-Hasura-Admin-Secret", config.HasuraAdminSecret)
+
+	var response InsertSessionResponse
+	err := hasuraClient.Run(context.Background(), req, &response)
+
+	if err != nil {
+		log.Panicf("Error creating session: %v", err)
+	}
+
+	return createJwtToken(
+		response.InsertedSessions.Returning[0].Id,
+		input.OwnerId,
+		input.DefaultRole,
+		response.InsertedSessions.Returning[0].CreatedAt,
+		response.InsertedSessions.Returning[0].ExpiresAt)
+}
+
+func createJwtToken(sessionId string, ownerId string, defaultRole string, issuedAt string, expiresAt string) string {
+	claims := Claims{
+		Hasura: HasuraClaims{
+			AllowedRoles: config.HasuraRoles,
+			DefaultRole:  defaultRole,
+			SessionId:    sessionId,
+			OwnerId:      ownerId,
+		},
+	}
+
+	issuedAtTime, _ := time.Parse(time.RFC3339, issuedAt)
+	expiresAtTime, _ := time.Parse(time.RFC3339, expiresAt)
+
+	claims.Id = sessionId
+	claims.Issuer = "session-graphql-actions"
+	claims.IssuedAt = issuedAtTime.Unix()
+	claims.ExpiresAt = expiresAtTime.Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
+	tokenString, err := token.SignedString([]byte(config.JwtKey))
+
+	if err != nil {
+		log.Panicf("Error creating jwt token: %v", err)
+	}
+
+	return tokenString
+}
